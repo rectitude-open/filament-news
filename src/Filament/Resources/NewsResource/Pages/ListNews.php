4823ad729fde55c17470e1357d54d6116abf3e84
aspirantzhang <admin@aspirantzhang.com>
2025-05-28 19:23:48 +0800

    feat: add create action to ListNews header

--- src/Filament/Resources/NewsResource/Pages/ListNews.php
+++ src/Filament/Resources/NewsResource/Pages/ListNews.php
@@ -4,10 +4,18 @@ declare(strict_types=1);
 
 namespace RectitudeOpen\FilamentNews\Filament\Resources\NewsResource\Pages;
 
+use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 use RectitudeOpen\FilamentNews\Filament\Resources\NewsResource;
 
 class ListNews extends ListRecords
 {
     protected static string $resource = NewsResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make(),
+        ];
+    }
 }
