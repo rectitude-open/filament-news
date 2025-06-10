03d2ffb779840106bce7e80d6dad66c02eb644b4
aspirantzhang <admin@aspirantzhang.com>
2025-06-10 14:49:45 +0800

    feat: add revisions action to EditNews page

--- src/Filament/Resources/NewsResource/Pages/EditNews.php
+++ src/Filament/Resources/NewsResource/Pages/EditNews.php
@@ -6,6 +6,7 @@ namespace RectitudeOpen\FilamentNews\Filament\Resources\NewsResource\Pages;
 
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
+use Mansoor\FilamentVersionable\Page\RevisionsAction;
 use RectitudeOpen\FilamentNews\Filament\Resources\NewsResource;
 
 class EditNews extends EditRecord
@@ -15,6 +16,7 @@ class EditNews extends EditRecord
     protected function getHeaderActions(): array
     {
         return [
+            RevisionsAction::make(),
             Actions\DeleteAction::make(),
             Actions\ForceDeleteAction::make(),
             Actions\RestoreAction::make(),
