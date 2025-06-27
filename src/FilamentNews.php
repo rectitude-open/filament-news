28a49fc8aad964899d2947199f3fb3547b718274
aspirantzhang <admin@aspirantzhang.com>
2025-06-27 14:52:55 +0800

    feat: implement getModel method to return configured news model class

--- src/FilamentNews.php
+++ src/FilamentNews.php
@@ -2,4 +2,16 @@
 
 namespace RectitudeOpen\FilamentNews;
 
-class FilamentNews {}
+use Illuminate\Database\Eloquent\Model;
+use RectitudeOpen\FilamentNews\Models\News;
+
+class FilamentNews
+{
+    /**
+     * @return class-string<Model>
+     */
+    public function getModel(): string
+    {
+        return config('filament-news.news.model', News::class);
+    }
+}
