91e6841e4c7a9b96c51efbc1b99a5b8cae719fd4
aspirantzhang <admin@aspirantzhang.com>
2025-06-27 14:52:32 +0800

    fix: add created_at to versionable fields and implement query scopes for slug, active, and suspended statuses

--- src/Models/News.php
+++ src/Models/News.php
@@ -6,6 +6,7 @@ namespace RectitudeOpen\FilamentNews\Models;
 
 use Awcodes\Curator\Models\Media;
 use Cviebrock\EloquentSluggable\Sluggable;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -16,6 +17,21 @@ use RalphJSmit\Laravel\SEO\Support\HasSEO;
 use RectitudeOpen\FilamentNews\Database\Factories\NewsFactory;
 use Spatie\Tags\HasTags;
 
+/**
+ * @property int $id
+ * @property string $title
+ * @property string $slug
+ * @property string $summary
+ * @property string $content
+ * @property int $weight
+ * @property int $featured_image_id
+ * @property int $status
+ * @property \Illuminate\Support\Carbon $created_at
+ * @property \Illuminate\Support\Carbon $updated_at
+ * @property \Illuminate\Support\Carbon|null $deleted_at
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \RectitudeOpen\FilamentNews\Models\NewsCategory> $categories
+ * @property-read int|null $categories_count
+ */
 class News extends Model
 {
     use HasFactory;
@@ -27,7 +43,7 @@ class News extends Model
 
     protected $fillable = ['title', 'slug', 'summary', 'content', 'weight', 'featured_image_id', 'status', 'created_at'];
 
-    protected $versionable = ['title', 'slug', 'summary', 'content', 'weight', 'featured_image_id', 'status'];
+    protected $versionable = ['title', 'slug', 'summary', 'content', 'weight', 'featured_image_id', 'status', 'created_at'];
 
     protected $versionStrategy = VersionStrategy::SNAPSHOT;
 
@@ -55,6 +71,27 @@ class News extends Model
     public function featured_image(): BelongsTo
     {
         return $this->belongsTo(Media::class, 'featured_image_id', 'id');
+    }
+
+    // @phpstan-ignore-next-line
+    #[Scope]
+    protected function withSlug(Builder $query, string $slug): void
+    {
+        $query->where('slug', $slug);
+    }
+
+    // @phpstan-ignore-next-line
+    #[Scope]
+    public function active(Builder $query): void
+    {
+        $query->where('status', 1);
+    }
+
+    // @phpstan-ignore-next-line
+    #[Scope]
+    public function suspended(Builder $query): void
+    {
+        $query->where('status', 0);
     }
 
     protected static function newFactory()
