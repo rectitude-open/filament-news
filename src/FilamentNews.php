56a7893b2b2f62281fecccc75cc882bfa52337ba
aspirantzhang <admin@aspirantzhang.com>
2025-07-08 20:34:59 +0800

    feat: add getLatestActiveNewsPaginated method in FilamentNews

--- src/FilamentNews.php
+++ src/FilamentNews.php
@@ -2,16 +2,25 @@
 
 namespace RectitudeOpen\FilamentNews;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 use RectitudeOpen\FilamentNews\Models\News;
 
 class FilamentNews
 {
     /**
-     * @return class-string<Model>
+     * @return class-string<News>
      */
     public function getModel(): string
     {
         return config('filament-news.news.model', News::class);
+    }
+
+    /**
+     * @return LengthAwarePaginator<News>
+     */
+    public function getLatestActiveNewsPaginated(int $perPage = 10): LengthAwarePaginator
+    {
+        // @phpstan-ignore-next-line
+        return $this->getModel()::latest()->published()->paginate($perPage);
     }
 }
